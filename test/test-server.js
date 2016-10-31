c9a3fc245298ae02c344fec5036891ba296ba4e3
kateah1 <kate.hurd64@gmail.com>
2016-10-31 13:46:54 -0400

    update server

--- test/test-server.js
+++ test/test-server.js
@@ -1,6 +1,6 @@
 var chai = require('chai');
 var chaiHttp = require('chai-http');
-var server = require('../server/app');
+var server = require('../server/server.js');
 var should = chai.should();
 
 chai.use(chaiHttp);
